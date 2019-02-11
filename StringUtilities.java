737634df06bad512ede39aa7c2c7219b3e88d8d1
Cara Eppes <caraeppes@zipcoders-MacBook-Pro-4.local>
2019-02-11 10:17:10 -0500

    completed all StringUtilities methods and pass all tests

--- StringUtilities.java
+++ StringUtilities.java
@@ -5,7 +5,7 @@ public class StringUtilities {
      * @return `input`
      */
     public String returnInput(String input) {
-        return null;
+        return input;
     }
 
     /**
@@ -14,7 +14,7 @@ public class StringUtilities {
      * @return the concatenation of `baseValue` and `valueToBeAdded`
      */
     public String concatenate(String baseValue, String valueToBeAdded) {
-        return null;
+        return baseValue + valueToBeAdded;
     }
 
     /**
@@ -22,7 +22,12 @@ public class StringUtilities {
      * @return identical string with characters in opposite order
      */
     public String reverse(String valueToBeReversed) {
-        return null;
+        String[] s = valueToBeReversed.split("");
+        String answer = "";
+        for (int i = 0; i < s.length; i++){
+            answer = s[i] + answer;
+        }
+        return answer;
     }
 
     /**
@@ -30,7 +35,7 @@ public class StringUtilities {
      * @return middle character of `word`
      */
     public Character getMiddleCharacter(String word) {
-        return null;
+        return word.charAt((int)Math.floor(word.length() / 2));
     }
 
     /**
@@ -39,7 +44,7 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
-        return null;
+        return value.replaceAll(charToRemove.toString(), "");
     }
 
     /**
@@ -47,6 +52,7 @@ public class StringUtilities {
      * @return last `word` in sentence
      */
     public String getLastWord(String sentence) {
-        return null;
+        int lastSpace = sentence.lastIndexOf(" ");
+        return sentence.substring(lastSpace + 1);
     }
 }
